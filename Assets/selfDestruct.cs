4073541aac0b1b0284346ed9f4d4207593c51086
zaynabj <43301520+zaynabj@users.noreply.github.com>
2020-10-17 16:55:36 -0500

    added physics and lifesize scene

---
+++ Assets/selfDestruct.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selfDestruct : MonoBehaviour
+{
+	public float lifetime = 20.0f; // seconds
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (lifetime > 0){
+      	lifetime -= Time.deltaTime;
+      	if (lifetime <= 0) {
+      		Destroy(this.gameObject);
+      	}
+      }  
+    }
+}
