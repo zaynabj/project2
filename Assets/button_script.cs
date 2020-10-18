3bf2a46aeb5fb2a74d047ba3772e1e93548c249d
zaynabj <43301520+zaynabj@users.noreply.github.com>
2020-10-17 19:26:25 -0500

    added second animation and sound

--- Assets/button_script.cs
+++ Assets/button_script.cs
@@ -6,19 +6,24 @@ using Vuforia;
 public class button_script : MonoBehaviour
 {
     public GameObject vButton;
-    public Animator talkAnim;
+    public Animator anim;
+    public AudioSource audioSource;
+    //public float delay = 1;
+
     // Start is called before the first frame update
     void Start()
     {
         vButton = GameObject.Find("VirtualButton");
         vButton.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
         vButton.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
-        talkAnim.GetComponent<Animator>();
+        anim.GetComponent<Animator>();
     }
 
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
         //talkAnim.Play("talk");
+        anim.SetTrigger("buttonPressed");
+        audioSource.Play();
         Debug.Log("Button pressed");
     }
 
